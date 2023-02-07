d7b53f2fbadd8661e799532c7349e9962430429c
GRMARTM <120740684+GRMARTM@users.noreply.github.com>
2023-02-07 19:32:06 +0300

    Домашние задание к 6 семинару

---
+++ HW/Task_41/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[n];
+int i = 0;
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+    array[i] = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+}
+
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine($"Элемент [{i}]: {array[i]}");
+}
+
+int count = 0;
+Console.Write($"Положительные элементы: ");
+for (i = 0; i < n; i++)
+{
+    if (array[i] > 0) 
+    {
+       
+       Console.Write($"{array[i]}, ");
+       
+       count++; 
+    }
+}
+Console.Write("\b\b ");
+Console.WriteLine($" Количество элементов больше нуля равно: {count}");
